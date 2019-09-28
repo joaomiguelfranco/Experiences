9c6cb15ef79a7ac053d15631a840179ed2ef732a
jfranco <joaofranco.pt@gmail.com>
2019-09-28 19:16:43 +0200

    Abstract Factory Class in cpp

---
+++ cpp/abstractFactoryForSorting.cpp
@@ -0,0 +1,143 @@
+// Disclaimer:
+//  - I used sorting algorithms from the web
+//  - I didn't implement my ones, since:
+//        - The exercise is planned for 1h
+//        - And I focused on the Abstract Factory and code readability
+//  - In the sorting method decision:
+//        - I took the liberty to use Enum Classes instead of the string, because:
+//            - It is strong typed
+//            - Doesn't give room for human errors (e.g. case-sensitivy)
+//        - If string is really required, I'd create an adapter between the string and the Enum Class SortingMethod.
+//  - The only tests are in main() and they are really simple
+//  - In production code, I'd invest more in:
+//      - UTs in the SortingAlgorithms
+//      - UT for the factory with a mocked SortAlgo to check for expectations on which algo is called depending on the input for the factory.
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+
+class SortAlgo
+{
+public:
+  virtual ~SortAlgo() = default;
+  virtual void sort(std::vector<int>&) = 0;
+};
+
+
+enum class SortingMethod
+{
+  BUBBLE,
+  QUICK,
+  HEAP
+};
+
+class BubbleSortAlgo : public SortAlgo
+{
+public:
+  void sort(std::vector<int>& input_list) 
+  {
+    bubbleSort(input_list);
+  }
+    
+private:
+  // A function to implement bubblesort. I picked it from the web
+  void bubbleSort(std::vector<int>& list)  
+  {  
+    for (int i = 0; i < list.size()-1; i++)
+    {
+      for (int j = 0; j < list.size()-i-1; j++)  
+        if (list.at(j) > list.at(j+1))  
+          std::swap(list.at(j), list.at(j+1));  
+    }
+  }  
+
+};
+
+class HeapSortAlgo : public SortAlgo
+{
+public:
+  void sort(std::vector<int>& input_list) 
+  {
+    heapSort(input_list);
+  }
+    
+private:
+  void heapify(std::vector<int>& array, int len, int i)
+  {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < len && array[left] > array[largest])
+      largest = left;
+
+    if (right < len && array[right] > array[largest])
+      largest = right;
+
+    if (largest != i)
+    {
+      std::swap(array[i], array[largest]);
+
+      heapify(array, len, largest);
+    }
+  }
+  
+  // To be honest, I picked an already existing sorting algo from the web. Didn't implement my own version.
+  void heapSort(std::vector<int>& input)
+  {
+    for (int i = input.size() / 2 - 1; i >= 0; i--)
+      heapify(input, input.size(), i);
+
+    for (int i = input.size() - 1; i >= 0; i--)
+    {
+      std::swap(input[0], input[i]);
+      heapify(input, i, 0);
+    }
+  }
+
+
+};
+
+void sortFactoryMethod(
+    const SortingMethod& sortingMethod,
+    std::vector<int>& input_list)
+{
+  std::unique_ptr<SortAlgo> abstractSortMethod;
+  
+  switch(sortingMethod)
+  {
+    case(SortingMethod::BUBBLE):
+      abstractSortMethod = std::make_unique<BubbleSortAlgo>();
+      abstractSortMethod->sort(input_list);
+      break;
+    case(SortingMethod::HEAP):
+      abstractSortMethod = std::make_unique<HeapSortAlgo>();
+      abstractSortMethod->sort(input_list);
+      break;
+    default: throw ("Sorting algorithm not implemented");    
+  }
+    
+}
+
+void print_list(const std::vector<int>& list)
+{
+  for(const auto& i : list) std::cout << i << ' ';
+  std::cout << std::endl;
+}
+
+int main()
+{  
+  std::vector<int> input_list = {4,5,1,643,23,123,542,5243};
+    
+  std::cout << "BUBBLE" << std::endl;http://cpp.sh/#
+  print_list(input_list);
+  sortFactoryMethod(SortingMethod::BUBBLE, input_list);
+  print_list(input_list);
+
+  std::cout << "\nHEAP" << std::endl;
+  input_list = {213,5342,12,22,1,4,451,29};
+  print_list(input_list);
+  sortFactoryMethod(SortingMethod::HEAP, input_list);
+  print_list(input_list);
+}
